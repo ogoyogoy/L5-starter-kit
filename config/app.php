59b1276dbc1e5066de02095f3e480ba027139b9d
Michael Huang <mbr.michael@gmail.com>
2016-08-19 13:33:24 +0700

    Add Intervention Image

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers
          */
         Cartalyst\Sentinel\Laravel\SentinelServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -210,6 +211,7 @@ return [
         'Activation' => Cartalyst\Sentinel\Laravel\Facades\Activation::class,
         'Reminder' => Cartalyst\Sentinel\Laravel\Facades\Reminder::class,
         'Sentinel' => Cartalyst\Sentinel\Laravel\Facades\Sentinel::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
