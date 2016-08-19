22b28912d26d4f338e578a8faeae8e5284b3d9e1
Michael Huang <mbr.michael@gmail.com>
2016-08-19 17:52:24 +0700

    Update site.php

--- config/site.php
+++ config/site.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'title'       => 'Site Title',
-    'description' => 'Site description here.',
+    'title'         => 'Site Title',
+    'description'   => 'Site description here.',
+    'name'          => 'Site Name',
+    'default_image' => '',
 ];
